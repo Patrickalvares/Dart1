d9d58c3afec0a710b5bda885e3f428d3e97a3664
Patrick Alvares <102553332+Patrickalvares@users.noreply.github.com>
2022-08-31 00:34:19 +0000

    completion of the first part of the dart course

--- main.dart
+++ main.dart
@@ -1,22 +1,57 @@
-void main(){
-	int idade1 = 26;
-	double idade2 = 24.85555;
-	double idade3 = idade1 + idade2;
-	print("a soma das idades é $idade3\n");
-	bool teste = false;
-	String resultado = 'Deu Certo!';
-	String nome = ' Patrick';
-	
-	List<dynamic> listaDinamica = [resultado, nome, teste];
-	
-	if(idade3 > 50){
-		teste = true;
-	}
-	if(teste){
-		print('${listaDinamica[0]}\n Eu sou ${listaDinamica[1]}\n ${listaDinamica[2]}');
-	}
+void main() {
 
-	List<String> listaNomes = ['Patrick', 'Luana', 'Beatriz', 'Pompilio'];
+  /// Comentário de Documentação
+  /** Novo Comentário de Multiplas linhas **/
 
-	print(listaNomes.reversed);
+
+  // Informações sobre a nossa Persona
+  int idade = 27;
+  double altura = 1.86;
+  bool geek = true;
+  const String nome = 'Patrick Alvares';
+  final String apelido;
+  apelido = 'Patrick';
+  bool maiorDeIdade;
+  int energia = 100;
+
+
+  // Método para definir se a Persona é maior de idade
+  if (idade >= 18) {
+    maiorDeIdade = true;
+  } else {
+    maiorDeIdade = false;
+  }
+
+  // Método para dizer quantas voltas foram dadas
+  for (int i = 1; i < 5; i = i + 2) {
+    print('Concluí $i voltas');
+  }
+
+  // Método para retirar a energia da Persona
+  while (energia > 0) {
+    print('Mais uma Repetição');
+    energia = energia - 6;
+  }
+
+  ///Exemplo de outra forma de usar o While
+  // do {
+  //   print("Mais uma Repetição");
+  //   energia = energia - 6;
+  // } while (energia > 0);
+
+  // Definimos nossa Lista com todos os Status da Persona
+  List<dynamic> patrick = [idade, altura, geek, nome, apelido];
+
+  //Frase que recebe os dados diretamente da Lista
+  String frase = 'Eu sou ${patrick[4]} \n'
+      'mas meu nome completo é: ${patrick[3]}, \n'
+      'eu me considero geek? ${patrick[2]}. \n'
+      'Eu tenho ${patrick[1]} metros de altura e \n'
+      '${patrick[0]} anos de idade,\n'
+      'Eu sou maior de idade? $maiorDeIdade';
+
+  //List<String> listanomes = ['Ricarth', 'Natália', 'Alex', 'Ândriu', 'André'];
+  //TODO: Fazer outras Personas da equipe de Mobile
+
+  print(frase);
 }
